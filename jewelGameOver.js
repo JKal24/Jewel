313e604a314a3ba044847c1c05cb6f05a14709f8
JKal24 <jkalirai2499@gmail.com>
2020-05-04 13:00:11 -0400

    Improving game functions and debugging

---
+++ jewelGameOver.js
@@ -0,0 +1,13 @@
+function gameOver() {
+   clearInterval(iteration);
+   $(board).hide();
+   $("#endContainer").show();
+   board.remove();
+
+   board = document.createElement('div');
+   $(board).attr('id', "board");
+   document.body.appendChild(board);
+
+   positions = [];
+   id = 0;
+}
